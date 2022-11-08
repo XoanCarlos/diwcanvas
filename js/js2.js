5fa0632c6873140171b69494857d888bd8aba18d
jcarlos <jcarlosperezgonzalez@gmail.com>
2022-11-08 20:51:36 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	js/scripts.js

--- js/js2.js
+++ js/js2.js
@@ -18,17 +18,33 @@ function dibujatorre(){
     ctx3.fill();
     ctx3.moveTo(170, (canvas.height/2) - 50 );
     ctx3.lineTo(300, (canvas.height/2) - 50 );
-    ctx3.strokeStyle = 'red';
+    ctx3.strokeStyle = "red";
     ctx3.stroke();
     ctx3.closePath();
 }
 
-function muevecanon(event){
 
-    if (event.keyCode == 39){
-        console.log('pulsa derecha');
-    }
-    else if (event.keyCode == 37){
-        console.log('pulsa izquierda');
-    }
+
+function muevecanoni(movi){
+    ctx3.clearRect(0,0, canvas.width, canvas.height);
+    dibujatanque();
+    dibujatorre();
+    ctx3.moveTo(170, (canvas.height/2) - 50);
+    ctx3.lineTo(300, (canvas.height/2) - 50 + movi);
+    ctx3.strokeStyle = "red";
+    ctx3.stroke();
+    ctx3.closePath();
+    return movi;
+}
+
+function muevecanona(mova){
+    ctx3.clearRect(0,0, canvas.width, canvas.height);
+    dibujatanque();
+    dibujatorre();
+    ctx3.moveTo(170, (canvas.height/2) - 50);
+    ctx3.lineTo(300, (canvas.height/2) - 50 - mova);
+    ctx3.strokeStyle = "red";
+    ctx3.stroke();
+    ctx3.closePath();
+    return mova;
 }
